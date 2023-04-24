53dd67179f1bafa3d2a30d8861581a0acfc135c8
Jannik <72359748+jannikm00@users.noreply.github.com>
2023-04-24 16:33:14 +0200

    Update coincollect.ino

--- code/coincollect.ino
+++ code/coincollect.ino
@@ -55,107 +55,106 @@ void ledupdate() {
   led.show();
 }
 void ledupdatep1() {
-  for (int i = 100; i < 149; i++) {  //draw player 1
-
+  for (int i = 100; i < 149; i++) {  //draw Player 1
     if (i == 100 || i == 101||i==149||i==148) {
-if(i+n1 <0&&i+n1 > MAXLED){
+if(i+n1 > 0 && i+n1 < MAXLED){
       led.setPixelColor(i + n1, led.Color(255, 0, 0));}
     }
   }
 }
 void ledupdatep2() {
-  for (int i = 100; i < 149; i++) {  //draw player 2
-
+  for (int i = 100; i < 149; i++) {  //draw Player 2
     if (i == 100 || i == 101||i==149||i==148) {
-      led.setPixelColor(i + n2, led.Color(0, 255, 0));
+if(i+n2 > 0 && i+n2 < MAXLED){
+      led.setPixelColor(i + n2, led.Color(0, 255, 0));}
     }
   }
 }
 void ledupdatep3() {
-  for (int i = 100; i < 149; i++) {  //draw player 3
-
+  for (int i = 100; i < 149; i++) {  //draw Player 3
     if (i == 100 || i == 101||i==149||i==148) {
-      led.setPixelColor(i + n3, led.Color(0, 0, 255));
+if(i+n3 > 0 && i+n3 < MAXLED){
+      led.setPixelColor(i + n3, led.Color(0, 0, 255));}
     }
   }
 }
 void ledupdatep4() {
-  for (int i = 100; i < 149; i++) {  //draw player 4
-
+  for (int i = 100; i < 149; i++) {  //draw Player 4
     if (i == 100 || i == 101||i==149||i==148) {
-      led.setPixelColor(i + n4, led.Color(255, 255, 255));
+if(i+n4 > 0 && i*n4 < MAXLED){
+      led.setPixelColor(i + n4, led.Color(255, 255, 255));}
     }
   }
 }
 void checkpos() {
   //PLAYER 1 Position
-  if (analogRead(p1x) > 950)  //player 1 up
+  if (analogRead(p1x) > 950)  //Player 1 up
   {
     n1 = n1 + 48;
   }
-  if (analogRead(p1x) < 300)  //player 1 down
+  if (analogRead(p1x) < 300)  //Player 1 down
   {
     n1 = n1 - 48;
   }
-  if (analogRead(p1y) > 950)  //player 1 right
+  if (analogRead(p1y) > 950)  //Player 1 right
   {
     n1 = n1 - 1;
   }
-  if (analogRead(p1y) < 300)  //player 1 left
+  if (analogRead(p1y) < 300)  //Player 1 left
   {
     n1 = n1 + 1;
   }
 
   //PLAYER 2 Position
-  if (analogRead(p2x) > 950)  //player 2 up
+  if (analogRead(p2x) > 950)  //Player 2 up
   {
     n2 = n2 + 48;
   }
-  if (analogRead(p2x) < 300)  //player 2 down
+  if (analogRead(p2x) < 300)  //Player 2 down
   {
     n2 = n2 - 48;
   }
-  if (analogRead(p2y) > 950)  //player 2 right
+  if (analogRead(p2y) > 950)  //Player 2 right
   {
     n2 = n2 - 1;
   }
-  if (analogRead(p2y) < 300)  //player 2 left
+  if (analogRead(p2y) < 300)  //Player 2 left
   {
     n2 = n2 + 1;
   }
 
   //PLAYER 3 Position
-  if (analogRead(p3x) > 950)  //player 3 up
+  if (analogRead(p3x) > 950)  //Player 3 up
   {
     n3 = n3 + 48;
   }
-  if (analogRead(p3x) < 300)  //player 3 down
+  if (analogRead(p3x) < 300)  //Player 3 down
   {
     n3 = n3 - 48;
   }
-  if (analogRead(p3y) > 950)  //player 3 right
+  if (analogRead(p3y) > 950)  //Player 3 right
   {
     n3 = n3 - 1;
   }
-  if (analogRead(p3y) < 300)  //player 3 left
+  if (analogRead(p3y) < 300)  //Player 3 left
   {
     n3 = n3 + 1;
   }
 
   //PLAYER 4 Position
-  if (analogRead(p4x) > 950)  //player 4 up
+  if (analogRead(p4x) > 950)  //Player 4 up
   {
     n4 = n4 + 48;
   }
-  if (analogRead(p4x) < 300)  //player 4 down
+  if (analogRead(p4x) < 300)  //Player 4 down
   {
     n4 = n4 - 48;
   }
-  if (analogRead(p4y) > 950)  //player 4 right
+  if (analogRead(p4y) > 950)  //Player 4 right
   {
     n4 = n4 - 1;
   }
-  if (analogRead(p4y) < 300)  //player 4 left
+  if (analogRead(p4y) < 300)  //Player 4 left
   {
     n4 = n4 + 1;
   }
@@ -167,38 +166,36 @@ void coin() {
     led.setPixelColor(ran, led.Color(255, 255, 0));
     countcoin = 0;
   }
-
- //player 1 check coin
+//Player 1 check coin
     if (101 + n1 == ran || 100 + n1 == ran||149 + n1 == ran || 148 + n1 == ran) {
       score1++;
       Serial.println(score1);
       countcoin = dotdelay;
     }
-  for (int i = 0; i < MAXLED; i++) {  //player 2 check coin
+ //Player 2 check coin
     if (101 + n2 == ran || 100 + n2 == ran||149 + n1 == ran || 148 + n1 == ran) {
       score2++;
       Serial.println(score2);
       countcoin = dotdelay;
     }
-  }
-  for (int i = 0; i < MAXLED; i++) {  //player 3 check coin
+  
+//Player 3 check coin
     if (101 + n3 == ran || 100 + n3 == ran||149 + n1 == ran || 148 + n1 == ran) {
       score3++;
             Serial.println(score3);
       countcoin = dotdelay;
     }
-  }
-  for (int i = 0; i < MAXLED; i++) {  //player 4 check coin
+  
+//Player 4 check coin
     if (101 + n4 == ran || 100 + n4 == ran||149 + n1 == ran || 148 + n1 == ran) {
       score4++;
             Serial.println(score4);
       countcoin = dotdelay;
-    }
-  }
+    } 
 }
 
 void checkwin() {
-  if (score1 >= 5) {  //player1 win screen
+  if (score1 >= 5) {  //Player 1 win screen
     for (int i = 0; i < MAXLED; i++) {
       led.setPixelColor(i, led.Color(255, 0, 0));
     }
@@ -210,7 +207,7 @@ void checkwin() {
     score1 = 0;
     delay(1000);
   }
-  if (score2 >= 5) {  //player2 win screen
+  if (score2 >= 5) {  //Player 2 win screen
     for (int i = 0; i < MAXLED; i++) {
       led.setPixelColor(i, led.Color(0, 255, 0));
     }
@@ -222,7 +219,7 @@ void checkwin() {
     score1 = 0;
     delay(1000);
   }
-  if (score3 >= 5) {  //player3 win screen
+  if (score3 >= 5) {  //Player 3 win screen
     for (int i = 0; i < MAXLED; i++) {
       led.setPixelColor(i, led.Color(0, 0, 255));
     }
@@ -234,7 +231,7 @@ void checkwin() {
     score1 = 0;
     delay(1000);
   }
-  if (score4 >= 5) {  //player4 win screen
+  if (score4 >= 5) {  //Player 4 win screen
     for (int i = 0; i < MAXLED; i++) {
       led.setPixelColor(i, led.Color(255, 255, 255));
     }
